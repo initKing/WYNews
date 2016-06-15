ecd7e6b54a9f8690c6a5d25f9352ee0a1e3b6e72
CrazyHacker <king_to_King@163.com>
2016-06-15 11:50:46 +0800

    实现数据源、代理方法

--- WYNews/WYNews/Classes/Home/NewsListController/WYNewsListController.m
+++ WYNews/WYNews/Classes/Home/NewsListController/WYNewsListController.m
@@ -12,6 +12,8 @@
 static NSString *simpleCell = @"simpleCell";
 @interface WYNewsListController ()<UITableViewDelegate, UITableViewDataSource>
 
+@property (nonatomic, weak) UITableView *tableView;
+
 @end
 
 @implementation WYNewsListController
@@ -23,6 +25,27 @@ static NSString *simpleCell = @"simpleCell";
     [self setupUI];
 }
 
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleCell forIndexPath:indexPath];
+    
+    cell.textLabel.text = @(indexPath.row).description;
+    
+    return cell;
+}
+
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - setupUI
 - (void)setupUI {
     // 1. 创建tableView
     UITableView *tv = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
@@ -38,11 +61,15 @@ static NSString *simpleCell = @"simpleCell";
     tv.rowHeight = 100;
     
     // 4. 注册原型cell
-    [tv registerNib:[UINib nibWithNibName:@"" bundle:nil] forCellReuseIdentifier:simpleCell];
+#warning TODO 替换cell
+ //  [tv registerNib:[UINib nibWithNibName:@"" bundle:nil] forCellReuseIdentifier:simpleCell];
+   
+    [tv registerClass:[UITableViewCell class] forCellReuseIdentifier:simpleCell];
     
     // 5. 指定代理、数据源
     tv.delegate = self;
     tv.dataSource = self;
+    _tableView = tv;
     
 }
 
